62858fbf610528bc48240eb6be8d26c843102484
Roberto Russo <roberto.russo@aulab.it>
2024-10-01 10:13:54 +0200

    sito della 152 pronto per la messa online

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Messa online 152</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-color-custom-robbie">
+    <x-nav></x-nav>
+    {{$slot}}
+</body>
+</html>
